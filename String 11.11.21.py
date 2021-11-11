0000dc51e6a4460516d3bc8c683cf3818f0aaa8c
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2021-11-11 12:36:16 +0000

    Add files via upload

---
+++ String 11.11.21.py
@@ -0,0 +1,19 @@
+string = 'hello world'
+lenghtOfString = len(string)
+print(lenghtOfString)
+
+'''
+print(string[0])
+
+print(string[5])
+
+print(string[-1])
+
+for ch in string:
+    print (ch)
+ 
+for ch in range(0,len(string)):
+    print(string[ch])
+    '''
+for ch in range(0,len(string)):
+    print(ch)
