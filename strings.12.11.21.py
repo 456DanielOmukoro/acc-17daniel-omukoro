b300ae115b4c6004d16876b10489daa1bf0ce049
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2021-11-12 15:15:16 +0000

    Add files via upload

---
+++ strings.12.11.21.py
@@ -0,0 +1,14 @@
+string = ('Daniel Melissa')
+'''  
+print (string.upper() )
+
+print (string.lower() )
+
+print (string.count('a') )
+
+print (string.find('') )
+
+print (string.replace('D', 'M'),string.replace ('M','D') )
+'''
+
+string[0]'w'
