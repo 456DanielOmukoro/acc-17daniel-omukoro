ad88b54b216ae54089c0ac3c79a8666f7bb27856
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2021-11-19 15:15:19 +0000

    Add files via upload

---
+++ q2 list sheet 19.11.21.py
@@ -0,0 +1,24 @@
+list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+list2=[1,2,3,4,5]
+
+
+#del list[6]
+#print (list)
+
+
+#print (list.pop(9)
+#print (list.reverse())
+#print(list)
+
+#print(list.copy())
+#print(list)
+
+#print(list.sort())
+#print(list)
+#list2.sort(reverse=True)
+#print(list2)
+
+
+
+
+
