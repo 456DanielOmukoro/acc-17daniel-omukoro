26917148a2818b8edd041fc612ac6b33ccba1387
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2021-11-29 09:40:22 +0000

    Add files via upload

---
+++ list worksheet 29.11.21.py
@@ -0,0 +1,27 @@
+file10 = open('C:/Users/17DOmukoro.ACC/Downloads/10Random.txt', 'r')
+file100 = open('C:/Users/17DOmukoro.ACC/Downloads/100Random.txt' , 'r')
+
+min= 100000000000000
+max= 0
+'''
+for line in file10:
+    for item in line.split():
+        number = int(item)
+        if(number < min):
+            min= int(item)
+        if(number > max ):
+            max= int(item)
+'''
+range=(max-min)
+print(range)
+
+frequency 
+for line in file10:
+     list.append()
+
+print 
+
+
+
+
+
