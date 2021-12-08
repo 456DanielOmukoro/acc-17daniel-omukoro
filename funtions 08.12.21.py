ecb976f3bc7c898f4f9554a284ae56a429665e94
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2021-12-08 15:15:44 +0000

    Add files via upload

---
+++ funtions 08.12.21.py
@@ -0,0 +1,29 @@
+'''
+def addTwoNum():
+    num1 = int(input('Enter your first number:'))
+    num2 = int(input('Enter your second number:'))
+    if num1>num2:
+        maxNum = num1
+    else:
+        maxNum = num2
+    print('The max of your numbers is:', maxNum)
+
+        
+        
+        
+addTwoNum()
+'''
+def addTwoNum(n1,n2):
+    
+    
+    if num1>num2:
+        maxNum = num1
+    else:
+
+        maxNum = num2
+    print('The max of your numbers is:', maxNum)
+    #print('The max of your numbers is:', maxNum)
+num1 = int(input('Enter your first number:'))
+num2 = int(input('Enter your second number:'))
+
+addTwoNum(num1,num2)
