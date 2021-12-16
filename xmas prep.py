dd2c5d152cc51753c46de4658f1f4803b72c3bbd
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2021-12-16 12:35:44 +0000

    Add files via upload

--- xmas prep.py
+++ xmas prep.py
@@ -1,7 +1,7 @@
 file1 = open('DanielChristmasTest2021.txt', 'w')
 
 #Program 1 list [23,1,0,-12,48]
-
+'''
 list1 = [23,1,0,-12,48]
 add = 0
 for item in list1:
@@ -30,9 +30,24 @@ for item in list1:
 print(even) 
         
         
-#multply wiht product by product
-        
+#program 4 multiply with product by product
+product = 1
+for item in list1:
+    product*=item 
+    #product 
     
+
+print(product)
+'''
+
+
+#program 5 display numbers between two intervals using a for loop.
+#Example:Write out every 4th number between 1 and 20 inclusive
+
+for item in range (0,21,4):
     
+    print(item)
+   
+   
     
     
