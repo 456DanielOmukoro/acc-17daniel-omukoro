e3271afd20b8affc8a3c6f4f8984c385dcadb757
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2022-02-10 12:35:48 +0000

    Add files via upload

---
+++ wrk sheet 17 10.2.21.py
@@ -0,0 +1,20 @@
+'''
+print('Hello World')
+ 
+user=input('Please Enter Your Name:')
+#print ('Hello', user)
+
+if user == 'Alice' or user== 'Bob':
+    print('Hello',user)
+else:
+    print('Cannot find name')
+'''   
+counter=0
+n= int(input('Please enter a number')) 
+for i in range(1,n+1):
+    
+
+    counter=counter+i
+print(counter)
+
+
