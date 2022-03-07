b2a5585b6108cca757458ba7207029472b0d4f31
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2022-03-07 09:41:38 +0000

    Add files via upload

---
+++ pyplot.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+
+x=[1,2,3,4,5]
+fx=[3,5,9,8,7]
+
+#plt.plot(x,fx)
+
+#add title etc.
+
+
+plt.title('Test Graph')
+plt.xlabel('x axis label')
+plt.ylabel('y axis label')
+plt.scatter(x,fx)
+plt.show()
+
+
