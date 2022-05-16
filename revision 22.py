3869f72e4cd11d7c13dc6b78f6c6b66cdc1db176
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2022-05-16 09:41:06 +0100

    Add files via upload

---
+++ revision 22.py
@@ -0,0 +1,23 @@
+f = open("revison.txt","r")
+
+space=0
+num=0
+counter=0
+for line in f:
+    for i in line:
+        if i == ' ':
+            space+=1
+    
+print('The file contains',space,'amount of spaces')
+caps =
+words=0
+num=0
+counter=0
+for line in f:
+    for i in line:
+        if i == ' ':
+            space+=1
+    
+print('The file contains',space,'amount of spaces')
+
+
