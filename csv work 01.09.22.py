0dffa254eb66bfede5fd3179134aebcea3076507
Babatunde <92168080+456DanielOmukoro@users.noreply.github.com>
2022-09-04 07:59:54 +0100

    Add files via upload

--- csv work 01.09.22.py
+++ csv work 01.09.22.py
@@ -1,17 +1,18 @@
-f = open('data.csv','a','\n')
+import csv
+f = open('data.csv','w',newline='')
 
 
 
 header = ['firstname','lastname','phone number']
-body = ['Tom','Smith','22344033','\n','Sandra','Jones','22344033','\n','John','Jacob','55007788']
 Fname1 = ['Tom','Smith','22344033']
 Fname2 = ['Sandra','Jones','22344033']
 Fname3 = ['John','Jacob','55007788']
 
 
 
-db = data.writer(f)
-db.writewrow(header)
+db = csv.writer(f)
+
+db.writerow(header)
 db.writerow(Fname1)
 db.writerow(Fname2)
 db.writerow(Fname3)
